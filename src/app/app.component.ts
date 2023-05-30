bd7c5413dd496ce550a2e701b3a367046e46415a
navodit <navodit50@gmail.com>
2023-05-31 01:00:33 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'product-preview-card';
+export class AppComponent implements OnInit {
+  isDesktopView!: boolean;
+  constructor(private breakpointObserver: BreakpointObserver) {}
+  ngOnInit(): void {
+    this.breakpointObserver
+      .observe([Breakpoints.Handset])
+      .subscribe((result) => {
+        this.isDesktopView = !result.matches;
+      });
+  }
 }
